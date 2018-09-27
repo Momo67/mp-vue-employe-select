c0416abfecbe662fc554a39289396d535bcccfed
Maurice Pittet <maurice.pittet@lausanne.ch>
2018-09-27 15:56:02 +0200

    Added EmployeSelect component

---
+++ src/components/EmployeSelect.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <v-app>
+
+  </v-app>
+</template>
+
+<script>
+export default {
+  props: {},
+  data () {
+    return {}
+  },
+  watch: {},
+  methods: {}
+}
+</script>
+
+<style lang="css">
+</style>
