9d4082e6feda61c2d769411abf09cd7959caae30
Maurice Pittet <maurice.pittet@lausanne.ch>
2018-09-28 08:30:46 +0200

    Script for testing component with vue serve

---
+++ src/components/employe-select-lib.js
@@ -0,0 +1,41 @@
+import '@babel/polyfill'
+import Vue from 'vue'
+import '../plugins/vuetify'
+
+import EmployeSelect from './EmployeSelect.vue'
+
+// Vue.config.productionTip = false
+
+export function install (Vue) {
+  Vue.component('employe-select', EmployeSelect)
+}
+
+export {
+  EmployeSelect
+}
+
+/* -- Plugin definition & Auto-install -- */
+/* You shouldn't have to modify the code below */
+
+// Plugin
+const plugin = {
+  install
+}
+
+export default plugin
+
+// Auto-install
+let GlobalVue = null
+if (typeof window !== 'undefined') {
+  GlobalVue = window.Vue
+} else if (typeof global !== 'undefined') {
+  GlobalVue = global.Vue
+}
+
+if (GlobalVue) {
+  GlobalVue.use(plugin)
+}
+
+new Vue({
+  render: h => h(EmployeSelect)
+}).$mount('#app')
