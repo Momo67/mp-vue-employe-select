7164c1cd244871eb0a94e10eb70f100b28f94b56
Maurice Pittet <maurice.pittet@lausanne.ch>
2018-10-19 17:36:48 +0200

    Update of vuetify and vue-i18n packages

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import fr from 'vuetify/es5/locale/fr'
+import i18n from '../i18n'
 
 Vue.use(Vuetify, {
   iconfont: 'mdi',
   lang: {
     locales: { fr },
-    current: 'fr'
+    current: 'fr',
+    t: (key, ...params) => i18n.t(key, params)
   },
 })
