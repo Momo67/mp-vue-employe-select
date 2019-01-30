c6a213f7a43150cfe134d5e7799c25dbad311d81
Maurice Pittet <maurice.pittet@lausanne.ch>
2019-01-30 14:06:41 +0100

    Different URLs for dev and production

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const DEV = process.env.NODE_ENV === 'development'
-export const EMP_URL_AJAX = 'http://localhost/goeland/employe/ajax'
-export const ORGUNIT_URL_AJAX = 'http://localhost/goeland/uniteorg/ajax'
+export const EMP_URL_AJAX = DEV ? 'http://mygolux.lausanne.ch/goeland/employe/ajax' : 'http://localhost/goeland/employe/ajax'
+export const ORGUNIT_URL_AJAX = DEV ? 'http://mygolux.lausanne.ch/goeland/uniteorg/ajax' : 'http://localhost/goeland/uniteorg/ajax'
 export const EMPLOYEE_INIT = {
   nom: null,
   prenom: null,
